a7d0f214adfd5aaaffa7572ab43d9823cf6269e3
premysl.talich <premysl.talich.cleverlance.com>
2021-05-09 23:47:03 +0200

    new arch

---
+++ feature/photo-detail/src/main/java/eu/talich/waller/feature/photodetail/PhotoDetailFragment.kt
@@ -0,0 +1,117 @@
+package eu.talich.waller.presentation.photodetail
+
+import android.net.Uri;
+import android.content.Intent;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import eu.talich.waller.R
+import eu.talich.waller.databinding.FragmentPhotoDetailBinding
+import eu.talich.waller.presentation.common.extension.enterFullScreenMode
+import eu.talich.waller.presentation.common.extension.exitFullScreenMode
+import eu.talich.waller.presentation.common.extension.loadPhoto
+import eu.talich.waller.presentation.photodetail.ui.PhotoDetailCard
+import eu.talich.waller.presentation.photodetail.vm.PhotoDetailViewModel
+import eu.talich.waller.common.photo.model.PhotoLocation
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+import androidx.compose.foundation.layout.ExperimentalLayout
+
+@ExperimentalLayout
+class PhotoDetailFragment : Fragment(R.layout.fragment_photo_detail) {
+
+    private lateinit var binding: FragmentPhotoDetailBinding
+    private val args: PhotoDetailFragmentArgs by navArgs()
+    private val viewModel: PhotoDetailViewModel by viewModel { parametersOf(args.photo) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPhotoDetailBinding.inflate(inflater, container, false)
+
+        (activity as AppCompatActivity).enterFullScreenMode()
+
+        val view = binding.root
+
+        binding.photo.loadPhoto(
+            url = viewModel.photo.fullSizeUrl,
+            color = viewModel.photo.color,
+            blurHash = viewModel.photo.blurHash
+        )
+        binding.photo.maximumScale = 5f
+
+        observePhotoDetail()
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        (activity as AppCompatActivity).exitFullScreenMode()
+    }
+
+    fun onLocationClick(location: eu.talich.waller.common.photo.model.PhotoLocation) {
+        fun getUri(): String {
+            val hasGpsLocation = (location.latitude != null && location.longitude != null)
+            val hasCityAndCountry = (location.city != null && location.country != null)
+
+            return if (hasGpsLocation) {
+                "geo:${location.latitude},${location.longitude}?q=${location.latitude},${location.longitude}"
+            } else {
+                if (hasCityAndCountry) {
+                    "geo:0,0?q=${location.city}, ${location.country}"
+                } else {
+                    "geo:0,0?q=${location.city ?: location.country}"
+                }
+            }
+        }
+
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(getUri())
+        }
+        if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun onTagClick(id: String) {
+        viewModel.setNewSearchQuery(id)
+        findNavController().navigate(R.id.action_photoDetailFragment_to_mainFragment)
+    }
+
+    private fun observePhotoDetail() {
+        lifecycleScope.launch {
+            viewModel.photoDetail.collect { photoDetail ->
+                photoDetail?.let {
+                    binding.photoDetailCard.setContent {
+                        MaterialTheme {
+                            with(it) {
+                                PhotoDetailCard(
+                                    user,
+                                    description,
+                                    createdAt,
+                                    likes,
+                                    location,
+                                    tags,
+                                    ::onLocationClick,
+                                    ::onTagClick
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
